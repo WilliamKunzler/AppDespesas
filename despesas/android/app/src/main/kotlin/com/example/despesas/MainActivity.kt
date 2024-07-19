00f47ff69ebd209535daf94383059f15cbeb3a50
WilliamKunzler <wkwilliamkunzler@gmail.com>
2024-07-19 10:38:42 -0300

    opa

---
+++ despesas/android/app/src/main/kotlin/com/example/despesas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.despesas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ despesas/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TelaPrincipal());
+}
+
+
+class TelaPrincipal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
