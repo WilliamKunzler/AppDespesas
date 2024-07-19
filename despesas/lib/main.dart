71b8422f29a4cb23408e75ea8175283a2c6b5c59
WilliamKunzler <wkwilliamkunzler@gmail.com>
2024-07-19 11:26:26 -0300

    opa

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -6,14 +7,15 @@ void main() {
   ));
 }
 
-
 class TelaPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: Text("opa")),
+      appBar: AppBar(
+          leading: Image.asset("images/logo.png"),
+          backgroundColor: Color.fromARGB(91, 192, 199, 199)),
+      backgroundColor: Color.fromARGB(255, 192, 199, 199),
       body: null,
     );
   }
 }
-
